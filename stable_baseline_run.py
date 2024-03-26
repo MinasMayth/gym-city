e26d212a2423dd0ad0766dfc56eebd7974879cc3
minas <minasmayth@gmail.com>
2024-03-26 16:02:42 +0100

    Updated log path

--- stable_baseline_run.py
+++ stable_baseline_run.py
@@ -92,7 +92,7 @@ def main():
         parameter_string = "_".join([f"{key}={value}" for key, value in parameter_values.items()])
         ALICE_path = '/home/s3458717/data1/'
         log_path = os.path.join(ALICE_path, "logs", "baselines", "ExpandedAgentToolset", "CustomNetworkV1",
-                                "BaseReward",
+                                "NewReward",
                                 f"{parameter_string}_{current_datetime}")
         save_path = log_path
     elif algorithm == "ppo":
@@ -100,7 +100,7 @@ def main():
     else:
         ALICE_path = '/home/s3458717/data1/'
         log_path = os.path.join(ALICE_path, "logs", "baselines", "ExpandedAgentToolset", "CustomNetworkV1",
-                                "BaseReward", algorithm, current_datetime)
+                                "NewReward", algorithm, current_datetime)
         save_path = log_path
 
     if args.save:
